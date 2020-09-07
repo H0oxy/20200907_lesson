420364289555077aada3a5094e562fed0af610e0
Hooxy <xMolod0yx.Arseny@yandex.ru>
2020-09-07 20:12:59 +0500

    prac01

--- prac01.py
+++ prac01.py
@@ -1,12 +1,14 @@
+'''
 print('Сложение двух чисел')
 num1 = 5
 num2 = 6
 sum = num1 + num2
 print(sum)
+'''
 
 print('Самогенерирующийся пример сложения')
 import random
 a = random.randint(1, 99)
 b = random.randint(2, 98)
 c = a + b
-print(a, '+' b, '=' c)
+print( a, '+', b, '=', c )
