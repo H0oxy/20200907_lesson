5b3ffdd813f45a4e8945c0eccc8f07441ab7e10e
Hooxy <xMolod0yx.Arseny@yandex.ru>
2020-09-07 20:54:05 +0500

    Change operation(unfinished/deadlock)

--- prac01.py
+++ prac01.py
@@ -6,9 +6,29 @@ sum = num1 + num2
 print(sum)
 '''
 
-print('Самогенерирующийся пример сложения')
 import random
+
+print('Самогенерирующийся пример сложения')
 a = random.randint(1, 99)
 b = random.randint(2, 98)
 c = a + b
-print( a, '+', b, '=', c )
+print(a, '+', b, '=', c)
+
+print('Change operation: add(+), minus(-), multiplication(*), division(/)')
+operation = int(input('+,-.*,/.'))
+if operation == "+":
+    print(a, operatiom, b, '=', c)
+    while:
+    print('Please enter a correct operation')
+if operation == "-":
+    print(a, operatiom, b, '=', c)
+    else:
+    print('Please enter a correct operation')
+if operation == "*":
+    print(a, operatiom, b, '=', c)
+    else:
+    print('Please enter a correct operation')
+if operation == "/":
+    print(a, operatiom, b, '=', c)
+    else:
+    print('Please enter a correct operation')
